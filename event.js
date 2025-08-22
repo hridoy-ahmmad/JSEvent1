3006d4e047d5a18b96abbe9707c39db93a887754
hridoy-ahmmad <hridoyp338y@gmail.com>
2025-08-22 16:09:19 +0600

    add Purple color With different way

--- event.js
+++ event.js
@@ -1,16 +1,26 @@
- function yellowMaker (){
-            document.body.style.backgroundColor = 'yellow'
-        }
-        function redMaker(){
-            document.body.style.backgroundColor = 'red'
-        }
-        function defaultcolor(){
-            document.body.style.backgroundColor = ''
-        }
+function yellowMaker() {
+    document.body.style.backgroundColor = 'yellow'
+}
+function redMaker() {
+    document.body.style.backgroundColor = 'red'
+}
+function defaultcolor() {
+    document.body.style.backgroundColor = ''
+}
 
-        //  Another way 
+//  Another way 
 
 const blueMaker = document.getElementById('btn-blue');
-blueMaker.onclick = function blue (){
+blueMaker.onclick = function blue() {
     document.body.style.backgroundColor = 'blue';
-}    
+}
+
+
+// another system
+
+const purpleMaker = document.getElementById('purple-btn')
+
+function makePurple() {
+    document.body.style.backgroundColor = 'purple'
+}
+purpleMaker.onclick = makePurple;
