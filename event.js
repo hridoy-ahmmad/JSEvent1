f7d6b2643c689a6e422cb78982cbf61c4ba19ef9
hridoy-ahmmad <hridoyp338y@gmail.com>
2025-08-22 15:46:30 +0600

    code transfer to js file

---
+++ event.js
@@ -0,0 +1,9 @@
+ function yellowMaker (){
+            document.body.style.backgroundColor = 'yellow'
+        }
+        function defaultcolor(){
+            document.body.style.backgroundColor = ''
+        }
+        function redMaker(){
+            document.body.style.backgroundColor = 'red'
+        }
